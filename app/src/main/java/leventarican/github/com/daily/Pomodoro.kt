d45b6ec045e6597d5036e02f4bb2c60644425ebf
leventarican <levent@protonmail.com>
2019-03-22 23:45:52 +0100

    add some ruler motion

--- app/src/main/java/leventarican/github/com/daily/Pomodoro.kt
+++ app/src/main/java/leventarican/github/com/daily/Pomodoro.kt
@@ -7,9 +7,14 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import android.view.MotionEvent
+
+
 
 class Pomodoro : View {
 
+    private var viewWidth: Int = 0
+    private var value = 3  // 1..5
     private lateinit var rulerPaint: Paint
 
     constructor(context: Context?): super(context) {
@@ -29,15 +34,76 @@ class Pomodoro : View {
             this.color = Color.WHITE
             this.strokeWidth = 5F
         }
+
+        var down = 0
+        var motion = 0
+        setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    down = event.x.toInt()
+                }
+                MotionEvent.ACTION_UP -> {
+                    motion = event.x.toInt() - down
+                    if (motion < 300) {
+                        if ((value+1) <= 5) {
+                            value++
+                        }
+                    } else if (motion > 300) {
+                        if ((value-1) > 0) {
+                            value--
+                        }
+                    }
+                    postInvalidate()
+                    Log.d("#code#", "motion: $motion; value: $value")
+                }
+            }
+            true
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        viewWidth = w
+        Log.d("#code#", "width: $width; w: $w")
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        Log.d("#code#", "measuredHeight: $measuredHeight; measuredWidth: $measuredWidth; height: $height; width: $width")
-
-        for (i in 0..25 step 5) {
-            canvas?.drawLine(10F * i, 10F, 10F * i, 100F, rulerPaint)
+        /*
+        0 mins  > | | | | |
+        25 min  >         | | | | |
+        10 min  >     | | | | |
+         */
+        val steps = (width-100/*some padding*/) / 5
+        // TODO: refactor
+        when (value) {
+            1 -> {
+                for (i in 3..5) {
+                    canvas?.drawLine((steps * i).toFloat(), 10F, (steps * i).toFloat(), 100F, rulerPaint)
+                }
+            }
+            2 -> {
+                for (i in 2..5) {
+                    canvas?.drawLine((steps * i).toFloat(), 10F, (steps * i).toFloat(), 100F, rulerPaint)
+                }
+            }
+            3 -> {
+                for (i in 1..5) {
+                    canvas?.drawLine((steps * i).toFloat(), 10F, (steps * i).toFloat(), 100F, rulerPaint)
+                }
+            }
+            4 -> {
+                for (i in 1..4) {
+                    canvas?.drawLine((steps * i).toFloat(), 10F, (steps * i).toFloat(), 100F, rulerPaint)
+                }
+            }
+            5 -> {
+                for (i in 1..3) {
+                    canvas?.drawLine((steps * i).toFloat(), 10F, (steps * i).toFloat(), 100F, rulerPaint)
+                }
+            }
         }
     }
 
