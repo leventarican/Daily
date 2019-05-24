3ec06282a614d3b00fd062307109bffdf54a9702
leventarican <levent@protonmail.com>
2019-05-24 16:54:44 +0200

    add progress and pomodore content

--- app/src/main/java/leventarican/github/com/daily/panes/LogMaximizedFragment.kt
+++ app/src/main/java/leventarican/github/com/daily/panes/LogMaximizedFragment.kt
@@ -4,23 +4,53 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.motion.widget.MotionLayout
+import android.widget.ScrollView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.log_maximized_fragmentv1.*
 import leventarican.github.com.daily.R
 
 class LogMaximizedFragment : Fragment() {
-    private lateinit var motionLayout: MotionLayout
+    private lateinit var motionLayout: ScrollView
 
     companion object {
         fun newInstance() = LogMaximizedFragment()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.log_maximized_fragment, container, false)
+        return inflater.inflate(R.layout.log_maximized_fragmentv1, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        motionLayout = view.findViewById(R.id.log_maximized_fragment)
+//        motionLayout = view.findViewById(R.id.sv_log_maximized_fragment)
         super.onViewCreated(view, savedInstanceState)
+
+        txtLog.text = """
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+            [LOG] scroll this
+
+        """.trimIndent()
     }
 }
